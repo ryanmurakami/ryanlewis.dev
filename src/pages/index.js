76f5f6d56b58ce8c8bd9f5aeee2e0eea4ab58a82
ryanmurakami <ryanharrisonlewis@gmail.com>
2019-12-03 01:04:18 -0700

    just something quick

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => <>
+    <div>Ryan H Lewis</div><br />
+    <div><a href="https://twitter.com/ryanmurakami">Twitter - @ryanmurakami</a></div><br />
+    <div><a href="https://github.com/ryanmurakami">Github - @ryanmurakami</a></div><br />
+    <div><a href="https://app.pluralsight.com/profile/author/ryan-lewis">Pluralsight Profile</a></div><br />
+    <div>More Coming Soon!</div>
+  </>
